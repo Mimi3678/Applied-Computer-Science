2fb19d9c10d1a46d4d5fbcac316f9c395f15ea79
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2022-02-17 18:33:31 +0100

    Jobbet med JavaScript 1 module. Frontend

---
+++ src/main/resources/static/javascript1/js1.js
@@ -0,0 +1,20 @@
+//Oppgave 1
+function helloWorld() {
+    document.write("Hei verden!");
+}
+
+//Oppgave 2
+function helloAlert() {
+    alert("Hei verden!");
+}
+
+//Oppgave 3
+function write(data) {
+    document.write(data);
+}
+
+//Oppgave 4
+function visNavn(data) {
+    //siden det skal forandres, bruker vi datatypen let
+    alert(data)
+}
