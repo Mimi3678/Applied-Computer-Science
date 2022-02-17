5aeacbc0f91551564f83dd36bdf1490114429f35
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2022-02-17 15:44:33 +0100

    Initial commit

---
+++ src/test/java/com/mimidev/webprograming/WebProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimidev.webprograming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgramingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
