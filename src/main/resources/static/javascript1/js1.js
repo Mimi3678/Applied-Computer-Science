c0438db206b3aa9eb4d72a625b153d0a7b2a1a81
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2022-02-17 22:45:31 +0100

    endring i javascript1 ukesoppgaver. Frontend

--- src/main/resources/static/javascript1/js1.js
+++ src/main/resources/static/javascript1/js1.js
@@ -16,5 +16,45 @@ function write(data) {
 //Oppgave 4
 function visNavn(data) {
     //siden det skal forandres, bruker vi datatypen let
-    alert(data)
+    alert(data);
+}
+
+//oppgave 5
+function writeConsole(data) {
+    console.log(data);
+}
+
+//oppgave 6
+function inputFarge(input) {
+    input = input.fontcolor("red")
+    document.write(input);
+}
+
+//oppgave 7
+function inputStor(input) {
+    input = input.toUpperCase()
+    alert(input);
+}
+
+//oppgave 8
+const tall = 1;
+function visMelding() {
+    console.log(tall);
+}
+
+//oppgave 9
+const heltall = 1;
+function tellOpp() {
+    console.log(heltall)
+    console.count(heltall);
+}
+
+//oppgave 10
+let out = 0;
+
+function tellMelding() {
+    out += 1;
+    let melding1 = "Du har trykket på knappen "
+    let melding2 = "ganger."
+    console.log(melding1 + out + melding2);
 }
